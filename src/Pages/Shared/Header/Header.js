ed77aa84676a57f186dbe92cb79e0f597feab00b
rahabulislam1 <rahabulislam1@gmail.com>
2022-05-28 19:47:19 +0600

    Added Header Part

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -17,7 +17,32 @@ const Header = () => {
     }
     return (
         <div>
-
+            <Navbar collapseOnSelect expand="lg" bg="primary" sticky='top' variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">HR Collection</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link style={{ color: 'white' }} as={Link} to="/manageInventories">Manage Inventory</Nav.Link>
+                            <Nav.Link style={{ color: 'white' }} href="home#reviews">Review</Nav.Link>
+                            <Nav.Link style={{ color: 'white' }} href="blogs">Blogs</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {user ?
+                                <div>
+                                    <Navbar.Brand as={Link} to="/addItems">Add Item</Navbar.Brand>
+                                    <Button onClick={myItems} variant="primary">My Items</Button>
+                                    <Button onClick={logout} variant="primary">Log out</Button>
+                                </div>
+                                :
+                                <Nav.Link style={{ color: 'white' }} as={Link} to="/Login">
+                                    Login
+                                </Nav.Link>
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
